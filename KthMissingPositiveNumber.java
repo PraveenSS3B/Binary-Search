4ba89a47c1b751bffea336e8706b820f893b38dd
PraveenSS3B <115014596+PraveenSS3B@users.noreply.github.com>
2024-01-21 22:44:26 +0530

    T.C - O(n) ; A.S - O(1)

---
+++ KthMissingPositiveNumber.java
@@ -0,0 +1,16 @@
+class KthMissingPositiveNumber {
+    public int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(arr[i] <= k)
+            {
+                k++;
+            }
+        }
+
+        return k;
+
+    }
+}
