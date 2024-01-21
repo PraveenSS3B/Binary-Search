c2d330a898e065119322fde4f68361f0deaf6cfd
PraveenSS3B <115014596+PraveenSS3B@users.noreply.github.com>
2024-01-21 23:16:18 +0530

    T.C - O(logN) ; A.S - O(1)

--- KthMissingPositiveNumber.java
+++ KthMissingPositiveNumber.java
@@ -1,16 +1,25 @@
 class KthMissingPositiveNumber {
     public int findKthPositive(int[] arr, int k) {
-        int n = arr.length;
+        int n = arr.length, low = 0, high = n-1;
 
-        for(int i = 0; i < n; i++)
+        while(low <= high)
         {
-            if(arr[i] <= k)
-            {
-                k++;
-            }
-        }
+            int mid = low + (high - low) / 2;
 
-        return k;
+            int missing  = arr[mid] - (mid + 1);
+
+            if(missing < k)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+
+        }
+        // arr[high] + k - arr[high] + (high + 1);
+        return k + high + 1; // or low + k i.e [low = high + 1] at the end.
 
     }
 }
