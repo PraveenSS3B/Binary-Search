6d800e5a44a6a578425051c3c31ecec879cc1831
PraveenSS3B <115014596+PraveenSS3B@users.noreply.github.com>
2024-02-03 21:30:55 +0530

    TC: O(log(min(n1,n2))) ; A.S - O(1)

--- MedianOfTwoSortedArrays.java
+++ MedianOfTwoSortedArrays.java
@@ -2,89 +2,48 @@ class MedianOfTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-
+    
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+    
         int n = n1 + n2;
-
-        int index2 = n / 2;
-        int index1 = index2 - 1;
-
-        // [1, 3, 4, 7, 10, 12] [2, 3, 6, 15] 
-        // [1, 2, 3, 3, 4, 6, 7, 10, 12, 15] -> index1 = 4, index2 = 5
-
-        int i = 0, j = 0;
-
-        int ele1 = -1, ele2 = -1, c = 0;
-
-        while(i < n1 && j < n2)
-        {
-            if(nums1[i] < nums2[j])
-            {
-                if(c == index1)
-                {
-                    ele1 = nums1[i];
-                }
-
-                if(c == index2)
-                {
-                    ele2 = nums1[i];
-                }
-                i++;
-                c++;
+    
+        int low = 0, high = n1;
+    
+        int symmetry = (n1 + n2 + 1) / 2;
+    
+        while (low <= high) {
+          int mid1 = low + (high - low) / 2;
+          int mid2 = symmetry - mid1;
+    
+          int l1 = Integer.MIN_VALUE, l2 = l1;
+          int r1 = Integer.MAX_VALUE, r2 = r1;
+    
+          if (mid1 > 0) l1 = nums1[mid1 - 1];
+          if (mid2 > 0) l2 = nums2[mid2 - 1];
+    
+          if(n1 > mid1) r1 = nums1[mid1];
+          if(n2 > mid2) r2 = nums2[mid2];
+    
+          if (l1 <= r2 && l2 <= r1) {
+            if (n % 2 == 0) {
+              return (double)(Math.max(l1, l2) + Math.min(r1, r2)) / 2;
+            } else {
+              return Math.max(l1, l2);
             }
-            else
-            {
-                if(c == index2)
-                {
-                    ele2 = nums2[j];
-                }
-
-                if(c == index1)
-                {
-                    ele1 = nums2[j];
-                }
-                j++;
-                c++;
-            }
+          }
+    
+          else if(l1 > r2 && l2 <= r1)
+          {
+              high = mid1 - 1;
+          }
+    
+          else{
+              low = mid1 + 1;
+          }
+    
         }
-
-        while(i < n1)
-        {
-            if(c == index1)
-            {
-                ele1 = nums1[i];
-            }
-
-            if(c == index2)
-            {
-                ele2 = nums1[i];
-            }
-            i++;
-            c++;
-        }
-
-        while(j < n2)
-        {
-            if(c == index2)
-            {
-                ele2 = nums2[j];
-            }
-            if(c == index1)
-            {
-                ele1 = nums2[j];
-            }
-            j++;
-            c++;
-        }
-
-        if(n % 2 == 0)
-        {
-            return (double) (ele1 + ele2) / 2;
-        }
-
-        else
-        {
-            return ele2;
-        }
+    
+        return 0;
         
     }
 }
