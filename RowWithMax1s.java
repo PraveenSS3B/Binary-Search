dd8e6d19001d6e7b56f3f5b61552c7ddbe3f55b7
PraveenSS3B <115014596+PraveenSS3B@users.noreply.github.com>
2024-02-10 19:57:02 +0530

    T.C - O(n X logm) ; A.S - O(1)

--- RowWithMax1s.java
+++ RowWithMax1s.java
@@ -1,21 +1,45 @@
-public static int maximumOnesRow_BruteForce(List<List<Integer>> matrix, int n, int m)
-    {
-        int ans = -1, currMax = Integer.MIN_VALUE;
-        for(int i = 0; i < matrix.size(); i++)
-        {
-            int c = 0;
-            for(int j = 0; j < matrix.get(i).size();j++)
-            {
-                if(matrix.get(i).get(j) == 1)
-                {
-                    c++;
+public static int getOnesCount(ArrayList<Integer> list) {
+
+        if (list.get(0) == 1) {
+            list.size();
+        }
+
+        else if (list.get(list.size() - 1) == 0) {
+            return 0;
+        }
+
+        int low = 0, high = list.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (list.get(mid) == 1) {
+                if (mid != 0 && list.get(mid - 1) == 1) {
+                    high = mid - 1;
+                    continue;
                 }
+                return list.size() - mid;
             }
-            if(c > currMax)
+
+            else if (list.get(mid) < 1) {
+                if (mid != list.size() - 1 && list.get(mid + 1) == 1) {
+                    low = mid + 1;
+                    continue;
+                }
+                low = mid + 1;
+            }
+        }
+
+        return 0;
+    }
+
+    public static int maximumOnesRow(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+        int ans = -1, currMax = Integer.MIN_VALUE;
+        for (int i = 0; i < matrix.size(); i++) {
+            int onesCount = getOnesCount(matrix.get(i));
+            if(onesCount > currMax)
             {
-                currMax = c;
-                c = 0;
-                ans = i;
+              currMax = onesCount;
+              ans = i; 
             }
         }
         return ans;
